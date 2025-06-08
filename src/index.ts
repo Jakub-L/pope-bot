31165ba6c5be402586808a8b000c3a22e8455dde
Jakub L. <47220449+Jakub-L@users.noreply.github.com>
2025-06-08 22:28:20 +0100

    Create basic login behaviour

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
 import "dotenv/config";
+import { Client, Events, GatewayIntentBits } from "discord.js";
+
+const { DISCORD_TOKEN } = process.env;
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.once(Events.ClientReady, (readyClient: Client) => {
+  console.log(`Ready! Logged in as ${readyClient.user?.tag}`);
+});
+
+client.login(DISCORD_TOKEN);
