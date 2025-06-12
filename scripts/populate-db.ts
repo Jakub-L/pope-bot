7bc9e434026ae09e580a352d6c95fc07328a3740
Jakub L <47220449+Jakub-L@users.noreply.github.com>
2025-06-12 22:27:02 +0100

    Start work on fetching past messages from channels

--- scripts/populate-db.ts
+++ scripts/populate-db.ts
@@ -1,5 +1,11 @@
 import "dotenv/config";
-import { ChannelType, Client, Events, GatewayIntentBits } from "discord.js";
+import {
+  ChannelType,
+  Client,
+  Events,
+  GatewayIntentBits,
+  GuildTextBasedChannel
+} from "discord.js";
 import phash from "sharp-phash";
 
 import { Database } from "../src/utils";
@@ -15,23 +21,54 @@ const discordClient = new Client({
   ]
 });
 
+const fetchAllLinks = async (
+  channel: GuildTextBasedChannel,
+  maxMessages: number = 5_000
+): Promise<string[]> => {
+  let links: Set<string> = new Set();
+  let messageCount = 0;
+
+  // Create message pointer
+  let message = await channel.messages
+    .fetch({ limit: 1 })
+    .then(messagePage => (messagePage.size === 1 ? messagePage.at(0) : null));
+
+  while (message && messageCount < maxMessages) {
+    const messagePage = await channel.messages.fetch({ limit: 100, before: message.id });
+    messageCount += messagePage.size;
+    console.log(`Channel ${channel.name} | Fetched ${messageCount} messages`);
+
+    for (const message of messagePage.values()) {
+      const { content, embeds, attachments } = message;
+      links = links.union(
+        new Set([
+          ...embeds.map(embed => embed.url).filter(url => url !== null),
+          ...attachments.map(attachment => attachment.url).filter(Boolean),
+          ...(content.match(/https?:\/\/[^\s]+/g) || [])
+        ])
+      );
+    }
+
+    // Update our message pointer to be the last message on the page of messages
+    message = 0 < messagePage.size ? messagePage.at(messagePage.size - 1) : null;
+  }
+
+  return Array.from(links);
+};
+
 discordClient.once(Events.ClientReady, async () => {
-  const channels = Array.from(discordClient.channels.cache.values())
-    .filter(channel => channel.type === ChannelType.GuildText)
-    .map(channel => ({
-      id: channel.id,
-      guildId: channel.guildId,
-      name: channel.name
-    }));
+  const channels = Array.from(discordClient.channels.cache.values()).filter(
+    channel => channel.type === ChannelType.GuildText
+  );
 
   if (channels.length === 0) return;
   console.log(`Found ${channels.length} channels.`);
   await db.deleteAllImages();
   console.log("Cleared existing images from the database.");
-  console.log("Getting messages:");
 
-  for (const channel of channels) {
-    console.log(channel.name);
+  for (const channel of channels.slice(0, 10)) {
+    const links = await fetchAllLinks(channel as GuildTextBasedChannel);
+    console.log(`Found ${links.length} links in channel ${channel.name}.`);
   }
   process.exit(0);
 });
