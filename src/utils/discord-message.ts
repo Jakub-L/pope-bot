3a48ed4d8ab82f0705b32839608f9397e1a690f8
Jakub L <47220449+Jakub-L@users.noreply.github.com>
2025-06-12 21:55:32 +0100

    Update getReply to work with new format

--- src/utils/discord-message.ts
+++ src/utils/discord-message.ts
@@ -1,24 +1,31 @@
 import type { Image } from "../types";
 import { formatDiff } from "./datetime";
+import salutations from "../data/salutations.json";
 
-export const getLinkToMessage = (image: Image): string => {
-  return `https://discord.com/channels/${image.guild_id}/${image.channel_id}/${image.message_id}`;
+const randomSelection = <T>(array: T[]): T => {
+  return array[Math.floor(Math.random() * array.length)];
 };
 
-export const getReply = (images: Image[]): string => {
-  const lastPost = images[images.length - 1];
-  const firstPost = images[0];
-  const count = images.length;
+export const getReply = (image: Image): string => {
+  const {
+    count,
+    guild_id,
+    first_post_user_name,
+    first_post_timestamp,
+    first_post_channel_id,
+    first_post_message_id,
+    last_post_user_name,
+    last_post_timestamp
+  } = image;
+  const link = `https://discord.com/channels/${guild_id}/${first_post_channel_id}/${first_post_message_id}`;
 
-  const salutation = "O ty psotniku!";
+  const salutation = randomSelection(salutations);
   const countMessage = `Już to widziałem ${count} ${count === 1 ? "raz" : "razy"}!`;
-  const firstSeen = `Najpierw zapostował to ${firstPost.user_name} ${formatDiff(
-    firstPost.timestamp
-  )} [tutaj](${getLinkToMessage(firstPost)}).`;
+  const firstSeen = `Najpierw zapostował to ${first_post_user_name} ${formatDiff(
+    first_post_timestamp
+  )} [tutaj](${link}).`;
   const lastSeen =
-    count > 1
-      ? `A ostatnio ${lastPost.user_name} ${formatDiff(lastPost.timestamp)}.`
-      : "";
+    count > 1 ? `A ostatnio ${last_post_user_name} ${formatDiff(last_post_timestamp)}.` : "";
 
   return [salutation, countMessage, firstSeen, lastSeen].join(" ").trim();
 };
