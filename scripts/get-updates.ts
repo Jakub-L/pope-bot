971b2d35ca50720c79e3b966727fec21d029e32c
Jakub L <47220449+Jakub-L@users.noreply.github.com>
2025-06-17 15:16:10 +0100

    Add script to convert links to database updates

--- scripts/get-updates.ts
+++ scripts/get-updates.ts
@@ -1,8 +1,37 @@
-const updates: ImageUpdate[] = [];
-for (let i = 0; i < links.length; i++) {
-  if (i > 0 && i % 100 === 0) console.log(`Processed ${i} links...`);
-  const link = links[i];
-  const update = await getUpdate(link);
-  if (update) updates.push(update);
-}
-console.log(`Total updates to process: ${updates.length}`);
+import { appendFileSync, readFileSync, writeFileSync } from "fs";
+import { Link } from "../src/types";
+import { getUpdate } from "../src/utils";
+
+const main = async () => {
+  const file = readFileSync("./temp/links.json", "utf-8");
+  const links: Link[] = JSON.parse(file);
+
+  let errorCount = 0;
+  let isFirstWrite = true;
+
+  console.log(`Total links to process: ${links.length}`);
+  writeFileSync("./temp/updates.json", "[", "utf-8");
+
+  for (let i = 0; i < links.length; i++) {
+    if (i > 0 && i % 100 === 0) console.log(`Processed ${i}/${links.length} links...`);
+    if (i < 60550) continue;
+    const link = links[i];
+    try {
+      const update = await getUpdate(link);
+      if (update !== null) {
+        appendFileSync(
+          "./temp/updates.json",
+          `${isFirstWrite ? "" : ","}${JSON.stringify(update)}`,
+          "utf-8"
+        );
+        isFirstWrite = false;
+      }
+    } catch (error) {
+      console.log(`Errors: ${++errorCount}`);
+    }
+  }
+
+  appendFileSync("./temp/updates.json", "]", "utf-8");
+};
+
+main();
