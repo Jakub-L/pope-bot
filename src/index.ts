588f5df8e70449988be71aafd3fd605dfb24151a
Jakub L <47220449+Jakub-L@users.noreply.github.com>
2025-06-19 21:13:21 +0100

    Add some extra logging

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,11 @@ import { Client, Events, GatewayIntentBits } from "discord.js";
 
 import { Database, getLinks, getReply, getUpdate } from "./utils";
 
-const { DISCORD_TOKEN, DISCORD_EXCLUDED_USER_IDS = "" } = process.env;
+const {
+  DISCORD_TOKEN,
+  DISCORD_EXCLUDED_USER_IDS = "",
+  DISCORD_EXCLUDED_GUILD_IDS = ""
+} = process.env;
 
 const db = new Database();
 const discordClient = new Client({
@@ -15,18 +19,30 @@ const discordClient = new Client({
 });
 
 const excludedUsers = new Set(DISCORD_EXCLUDED_USER_IDS.split(",").map(id => id.trim()));
+const excludedGuilds = new Set(DISCORD_EXCLUDED_GUILD_IDS.split(",").map(id => id.trim()));
+
+discordClient.once(Events.ClientReady, async () => {
+  console.log(`Logged in as ${discordClient.user?.tag}`);
+});
 
 discordClient.on(Events.MessageCreate, async message => {
   if (message.author.bot) return;
+  if (excludedGuilds.has(message.guildId ?? "")) return;
+
+  console.log(`\nHandling message from ${message.author.tag}`);
 
   const links = getLinks(message);
   if (links.length === 0) return;
+
+  console.log(`Found ${links.length} links in the message.`);
 
   for (const link of links) {
     const update = await getUpdate(link);
     if (update) {
       const image = await db.addImage(update);
+      console.log("Added image to database");
       if (image.count > 1) {
+        console.log(`Image already exists, count: ${image.count}`);
         message.reply({ content: getReply(image, excludedUsers.has(message.author.id)) });
       }
     }
