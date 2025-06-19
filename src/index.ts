3427f6a3613fe5771bf620752b39acdf38d5921c
Jakub L <47220449+Jakub-L@users.noreply.github.com>
2025-06-19 23:17:10 +0100

    Include message extract in log

--- src/index.ts
+++ src/index.ts
@@ -29,20 +29,23 @@ discordClient.on(Events.MessageCreate, async message => {
   if (message.author.bot) return;
   if (excludedGuilds.has(message.guildId ?? "")) return;
 
-  console.log(`\nHandling message from ${message.author.tag}`);
+  console.log(`\n${new Date().toISOString()} | Message from ${message.author.tag}`);
+  console.log(
+    `\t${message.content.length > 50 ? message.content.slice(0, 50) + "..." : message.content}`
+  );
 
   const links = getLinks(message);
   if (links.length === 0) return;
 
-  console.log(`Found ${links.length} links in the message.`);
+  console.log(`\tFound ${links.length} links in the message.`);
 
   for (const link of links) {
     const update = await getUpdate(link);
     if (update) {
       const image = await db.addImage(update);
-      console.log("Added image to database");
+      console.log("\tAdded image to database");
       if (image.count > 1) {
-        console.log(`Image already exists, count: ${image.count}`);
+        console.log(`\tImage already exists, count: ${image.count}`);
         message.reply({ content: getReply(image, excludedUsers.has(message.author.id)) });
       }
     }
