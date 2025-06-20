62d44dce98a5e51f353abf5be90b57491d514d6b
Jakub L <47220449+Jakub-L@users.noreply.github.com>
2025-06-20 21:56:09 +0100

    Parallelise updating the database

--- scripts/update-db.ts
+++ scripts/update-db.ts
@@ -1,19 +1,27 @@
-import { readFileSync } from "fs";
+import { readdirSync, readFileSync } from "fs";
 import { ImageUpdate } from "../src/types";
 import { Database } from "../src/utils";
 
 import "dotenv/config";
 
+const BATCH_SIZE = 200;
+
 const db = new Database();
 
 const main = async () => {
-  const { updates }: { updates: ImageUpdate[] } = JSON.parse(
-    readFileSync("./temp/updates.json", "utf-8")
-  );
+  const fileName: string | undefined = readdirSync("./export")
+    .filter((fileName: string) => fileName.startsWith("updates-"))
+    .sort()
+    .pop();
+  if (!fileName) process.exit(1);
+  const file: string = readFileSync(`./export/${fileName}`, "utf-8");
+  const { updates }: { updates: ImageUpdate[] } = JSON.parse(file);
 
-  for (let i = 0; i < updates.length; i++) {
-    if (i > 0 && i % 100 === 0) console.log(`Processed ${i}/${updates.length} updates...`);
-    await db.addImage(updates[i]);
+  for (let i = 0; i < updates.length; i += BATCH_SIZE) {
+    const batch = updates.slice(i, i + BATCH_SIZE);
+    console.log(`Processing items ${i + 1} to ${i + BATCH_SIZE}...`);
+
+    await Promise.all(batch.map(update => db.addImage(update)));
   }
 };
 
