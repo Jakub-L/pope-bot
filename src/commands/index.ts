ecb1755a58a885a4057400b31bddebdf314eeb27
Jakub L. <47220449+Jakub-L@users.noreply.github.com>
2025-11-07 20:44:13 +0000

    Add duplicate of /get

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,5 +1,6 @@
 import ping from "./ping";
 import get from "./get";
+import papaj from "./papaj";
 import stats from "./stats";
 
-export default [ping, get, stats];
+export default [ping, get, stats, papaj];
---
+++ src/commands/papaj.ts
@@ -0,0 +1,12 @@
+import { ChatInputCommandInteraction, SlashCommandBuilder } from "discord.js";
+
+import { Database, popeGet } from "../utils";
+
+const get = {
+  data: new SlashCommandBuilder().setName("papaj").setDescription("Chwyta papieża!"),
+  async execute(interaction: ChatInputCommandInteraction, db: Database) {
+    await popeGet(interaction, db);
+  }
+};
+
+export default get;
