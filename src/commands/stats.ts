37a1b69a64c84d3096634eafe8dcd269a9fbce19
Jakub L. <47220449+Jakub-L@users.noreply.github.com>
2026-02-14 21:00:53 +0000

    Correct display of get statistics

--- src/commands/stats.ts
+++ src/commands/stats.ts
@@ -3,13 +3,15 @@ import { PopeGet } from "../types";
 
 import { Database } from "../utils";
 
-const mapStatToMessage = (title: string, stats: PopeGet[]) => {
+const mapStatToMessage = (
+  title: string,
+  stats: PopeGet[],
+  statField: "total_gets" | "get_streak"
+) => {
   if (stats.length === 0) return "";
   return [
     `## ${title}:`,
-    ...stats.map(
-      (stat, index) => `**${index + 1}.** ${stat.user_name} - ${stat.get_streak} getów`
-    )
+    ...stats.map((stat, index) => `**${index + 1}.** ${stat.user_name} - ${stat[statField]} getów`)
   ].join("\n");
 };
 
@@ -21,8 +23,8 @@ const stats = {
     const streaks = await db.getStats("get_streak");
     const totals = await db.getStats("total_gets");
 
-    const streakMessage = mapStatToMessage("Top 5 getów z rzędu", streaks);
-    const totalMessage = mapStatToMessage("Top 5 papież-getów", totals);
+    const totalMessage = mapStatToMessage("Top 5 papież-getów", totals, "total_gets");
+    const streakMessage = mapStatToMessage("Top 5 getów z rzędu", streaks, "get_streak");
 
     if (!streakMessage && !totalMessage) {
       interaction.reply({
@@ -33,7 +35,7 @@ const stats = {
     }
 
     interaction.reply({
-      content: `${mapStatToMessage("Top 5 getów z rzędu", streaks)}\n\n${mapStatToMessage("Top 5 papież-getów", totals)}`,
+      content: [totalMessage, streakMessage].filter(Boolean).join("\n\n"),
       flags: MessageFlags.Ephemeral
     });
   }
